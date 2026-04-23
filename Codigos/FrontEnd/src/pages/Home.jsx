60ca107b6480f0c0f192f30d37b3e6028faf474f
Guilherme Antonio Santos de Oliveira <guilhermegaso420@gmail.com>
2026-04-23 18:11:11 -0300

    Parte visual finalizada

--- Codigos/FrontEnd/src/pages/Home.jsx
+++ Codigos/FrontEnd/src/pages/Home.jsx
@@ -1,19 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Footer from "../components/footer";
-import Header from '../components/header'
+import Header from '../components/header';
 import styles from '../styles/home.module.css';
 
 
-import banner from '../assets/imagens/banner.png'
-import coxinha from '../assets/imagens/coxinha.png'
+import banner from '../assets/imagens/banner.png';
+import coxinha from '../assets/imagens/coxinha.png';
+import carrinho from '../assets/imagens/carrinho_de_compras.png'
 
 const Home = () => {
     const navigate = useNavigate();
 
+    const [categoria, setCategoria] = useState("salgados")
+
+    if(categoria == 'doces') {
+        const ativo = '${styles.ativo}';
+    }
 
     return (
         <>
             <Header />
+            <div className={styles.carrinho}>
+                <img src={carrinho} alt="carrinho de compras" />
+            </div>
             <section className={styles.section}>
                 <div className={styles.cardapio_title}>
                     <div>
@@ -30,42 +40,52 @@ const Home = () => {
             <main>
                 <div className={styles.categorias}>
                     <div>
-                        <div className={`${styles.categorias_itens_ponta_esquerda} ${styles.ativo}`}>Salgados</div>
-                        <div className={`${styles.categorias_itens}`}>Doces</div>
-                        <div className={`${styles.categorias_itens_ponta_direita}`}>Bebidas</div>
+                        <div onClick={() => setCategoria("salgados")} className={`${styles.categorias_itens_ponta_esquerda} ${categoria === "salgados" ? styles.ativo : ""}`}>Salgados</div>
+                        <div onClick={() => setCategoria("doces")} className={`${styles.categorias_itens} ${categoria === "doces" ? styles.ativo : ""}`}>Doces</div>
+                        <div onClick={() => setCategoria("bebidas")} className={`${styles.categorias_itens_ponta_direita} ${categoria === "bebidas" ? styles.ativo : ""}`}>Bebidas</div>
                     </div>
                 </div>
 
-                <div className={styles.cardapio}>
-
-                    <div className={styles.cardapio_item}>
-                        <div className={styles.cardapio_item_head}>
-                            <div className={`${styles.cardapio_item_head_qtd}`}>0</div>
-                            <img src={coxinha} alt="Imagem de um coxinha em cima de um prato branco"/>
-                            <h4>Coxinha de frango</h4>
-                        </div>
-                        <div className={styles.cardapio_item_mid}>
-                            <div>R$ 6,00</div>
-                            <div className={styles.cardapio_item_mid_qtd}>
-                                <div className={styles.cardapio_item_mid_seletores}>-</div>
-                                <span>1</span>
-                                <div className={styles.cardapio_item_mid_seletores}>+</div>
+                {categoria === "salgados" &&
+                    <div className={styles.cardapio}>
+                        <div className={styles.cardapio_item}>
+                            <div className={styles.cardapio_item_head}>
+                                <div className={`${styles.cardapio_item_head_qtd}`}>0</div>
+                                <img src={coxinha} alt="Imagem de um coxinha em cima de um prato branco" />
+                                <h4>Coxinha de frango</h4>
+                            </div>
+                            <div className={styles.cardapio_item_mid}>
+                                <div>R$ 6,00</div>
+                                <div className={styles.cardapio_item_mid_qtd}>
+                                    <div className={styles.cardapio_item_mid_seletores}>-</div>
+                                    <input type="number" value={1} />
+                                    <div className={styles.cardapio_item_mid_seletores}>+</div>
+                                </div>
+                            </div>
+                            <div className={styles.cardapio_item_bottom}>
+                                <button type="submit" className={styles.cardapio_item_bottom_add}>Adcionar ao carrinho</button>
+                                <button type="submit" className={styles.cardapio_item_bottom_reservar}>Reservar</button>
                             </div>
                         </div>
-                        <div className={styles.cardapio_item_bottom}>
-                            <div className={styles.cardapio_item_bottom_add}>Adcionar ao carrinho</div>
-                            <div className={styles.cardapio_item_bottom_reservar}>Reservar</div>
-                        </div>  
-                    </div>                   
-
+                    </div>
+                }
+                {categoria === "doces" && 
+                <div className={styles.cardapio}>
+                    <h2>Lista de Doces</h2>
                 </div>
+                }
+                {categoria === "bebidas" &&
+                <div className={styles.cardapio}>
+                    <h2>Lista de bebidas</h2>
+                </div>
+                }
 
                 <div className={styles.paginacao}>
-                    
-                        <div className={styles.paginacao_ativa}>1</div>
-                        <div>{">"}</div>
-                    
-                    
+
+                    <div className={styles.paginacao_ativa}>1</div>
+                    <div>{">"}</div>
+
+
                 </div>
             </main>
             < Footer />
