b97c1731bf6b3d385957234816e890148685f360
Breno <nobresilva360@gmail.com>
2026-04-23 21:54:03 -0300

    Inicializando a página de controle de estoque

---
+++ Codigos/FrontEnd/src/pages/ControleDeEstoque.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import styles from '../styles/ControleDeEstoque.modele.css';
+import Header from '../components/header';
+import icon1 from '../assets/Edit.png';
+import icon2 from '../assets/add_circle.png';
+import icon3 from '../assets/Trash.png';
+
+export default function ControleDeEstoque() {
+
+    return (
+        <>
+        </>
+    )
+}
