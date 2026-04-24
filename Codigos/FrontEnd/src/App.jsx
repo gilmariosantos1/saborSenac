715524e456ee03053b5ed2168dd2f00fca75b2ff
phe <phellipemoura87@gmail.com>
2026-04-24 18:52:27 -0300

    Add files via upload

--- Codigos/FrontEnd/src/App.jsx
+++ Codigos/FrontEnd/src/App.jsx
@@ -9,6 +9,7 @@ import '@fontsource/open-sans';
 import '@fontsource/open-sans/700.css';
 import '@fontsource/inter';
 import Consultapedido from './pages/consultapedido';
+import ConfirmarPedido from './pages/ConfirmarPedido';
 import '@fontsource/sanchez';
 
 
@@ -22,6 +23,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/sugestoes' element={<Sugestoes />} />
           <Route path='/consultapedido' element={<Consultapedido />} />
+          <Route path='/confirmarpedido' element={<ConfirmarPedido />} />
           <Route path='/painelAtendente' element={<PainelAtentende />} />
 
       </Routes>
