a0c405c692bb4ce92fabb4c1e58c286d01483cf8
gilmariosantos1 <gilmariosantos1@gmail.com>
2026-04-27 22:28:18 -0300

    Tela de Login
    Ajustando a tela de login. Falta inserir a imagem com a fachada do prédio do senac e posicionar o card corretamente.

--- Codigos/FrontEnd/src/pages/Login.jsx
+++ Codigos/FrontEnd/src/pages/Login.jsx
@@ -1,45 +1,61 @@
-import { useNavigate } from "react-router-dom";
-import Footer from "../components/footer";
-import Header from '../components/header'
-// import './Home.css';
+import logo from '../assets/imagens/logo_sabor_senac.svg'
+import './Login.css';
+import Header from '../components/header.jsx';
+import Footer from '../components/footer.jsx';
 
 const Login = () => {
-    const navigate = useNavigate();
 
 
-      return (
+    return (
       <>
-        <Header />
-        <main className="auth-page">  
-<div><img src="./assents/fundo.svg" alt="" /></div>
-          <form className="auth-form">
-            <div className="auth-input-group">
-                <label htmlFor="email">E-mail</label>
-                <input
-                    type="email"
-                    id="email"
-                    placeholder="exemplo@email.com">
-                </input>
-                    <br></br>
-                <label htmlFor="password">Senha</label>
-                <input
-                    type="password"
-                    id="password"
-                    required
-                  />
-                </div>
-                <button type="submit" > ENTRAR AGORA</button>
-            </form>
-                <p><a class="Não tem conta?" href="#">Crie </a></p>
-           </main>
-              < Footer />
-    
-      </>
+      <Header />
+        <main className="auth-page">
+          <div className="login-left" />
+          <div className="login-container">            
+            <div className="login-right">
+              <section className="login-card">
+                <img className="login-logo" src={logo} alt="logo_sabor_senac" />
+                <h1>Entre em sua conta</h1>
+                <p className="login-subtitle">Acesse para pedir seu lanche</p>
+
+                <form className="login-form">
+                  <div className="form-group">
+                    <label htmlFor="email">E-mail</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="Seu email@exemplo.com"
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="password">Senha</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Digite sua senha"
+                    />
+                  </div>
+
+                  <button type="submit" className="btn btn-login">
+                    ENTRAR AGORA
+                  </button>
+                </form>
+
+                <p className="login-footer">
+                  Não tem conta? <a href="#">Crie</a>
+                </p>
+              </section>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </>          
+      
     )
   }
       
-
-
-
 
   export default Login;
