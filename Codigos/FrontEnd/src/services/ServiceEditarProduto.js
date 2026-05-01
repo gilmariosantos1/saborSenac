d5982ab86a7ad2a5ad4139aeac949e93eb956e0f
gilmariosantos1 <gilmariosantos1@gmail.com>
2026-05-01 20:33:49 -0300

    Ajustando ServiceEditarProduto e services api
    Corrigindo implementação da service API e ajustando ServiceEditarProduto.

--- Codigos/FrontEnd/src/services/ServiceEditarProduto.js
+++ Codigos/FrontEnd/src/services/ServiceEditarProduto.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/produtos";
+import { API_URL } from "./api";
 
 export const editarProduto = async (id, dados) => {
   try {
-    const response = await axios.put(`${API_URL}/${id}`, dados);
+    const response = await axios.put(`${API_URL}produto/${id}`, dados);
     return response.data;
   } catch (error) {
     console.error("Erro ao editar produto:", error);
